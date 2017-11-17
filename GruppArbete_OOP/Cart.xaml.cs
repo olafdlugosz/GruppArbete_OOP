83b7b40890363d8d9575d0ad75653ec5dbe84449
waredrive <waredrive@laptop>
2017-11-17 16:30:01 +0100

    Added more exception handling in Cart.xaml.cs

--- GruppArbete_OOP/Cart.xaml.cs
+++ GruppArbete_OOP/Cart.xaml.cs
@@ -15,16 +15,20 @@ namespace GruppArbete_OOP
         private List<Item> _orderList = new List<Item>();
         private MainWindow _mainWindow;
 
-        public Cart(MainWindow mainWindow) {
+        public Cart(MainWindow mainWindow)
+        {
             this._mainWindow = mainWindow;
             InitializeComponent();
         }
 
-        public void AddToCart(Item item) {
+        public void AddToCart(Item item)
+        {
             item.Quantity = 0;
 
-            for (int i = 0; i < _orderList.Count; i++) {
-                if (_orderList[i].Identifier == item.Identifier) {
+            for (int i = 0; i < _orderList.Count; i++)
+            {
+                if (_orderList[i].Identifier == item.Identifier)
+                {
                     _orderList[i].Quantity++;
                     CartListBox.Items.Remove(_orderList[i]);
                     CartListBox.Items.Add(_orderList[i]);
@@ -38,58 +42,81 @@ namespace GruppArbete_OOP
 
         }
 
-        private void Print_Click(object sender, RoutedEventArgs e) {
-            PrintDialog printDlg = new PrintDialog();
-
-            bool? print = printDlg.ShowDialog();
-            if (print == true)
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+            try
             {
-                printDlg.PrintVisual(CartListBox, "Printing Order");
-                CartListBox.Items.Clear();
-            }
-
-        }
-
-        private void SaveToFile_Click(object sender, RoutedEventArgs e) {
-            if (_orderList.Count == 0)
-                return;
-            else {
-                SaveFileDialog saveList = new SaveFileDialog {
-                    Filter = " TXT-fil| *.txt",
-                    Title = "Spara Plocklistan",
-                    FileName = $"Plocklista - {DateTime.Now.ToString("yyyy.MM.dd kl. HH.mm")}"
-                };
-                if (saveList.ShowDialog() == true) {
-                    StreamWriter writer = new StreamWriter(saveList.OpenFile());
-                    writer.WriteLine($"Plocklistan skapad den {DateTime.Now.ToString("yyyy.MM.dd kl. HH:mm")}", false);
-                    writer.WriteLine("\n\n");
-                    writer.WriteLine($"Varor att Plocka:");
-
-                    for (int i = 0; i < _orderList.Count; i++) {
-                        writer.WriteLine(_orderList[i].ToString());
-                    }
-
-                    writer.Dispose();
-                    writer.Close();
-                    Environment.Exit(0);
+                PrintDialog printDlg = new PrintDialog();
+                bool? print = printDlg.ShowDialog();
+                if (print == true)
+                {
+                    printDlg.PrintVisual(CartListBox, "Printing Order");
+                    CartListBox.Items.Clear();
                 }
             }
+            catch
+            {
+                MessageBox.Show("There was a problem printing your orderlist!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SaveToFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_orderList.Count == 0)
+                    return;
+                else
+                {
+                    SaveFileDialog saveList = new SaveFileDialog
+                    {
+                        Filter = " TXT-fil| *.txt",
+                        Title = "Spara Plocklistan",
+                        FileName = $"Plocklista - {DateTime.Now.ToString("yyyy.MM.dd kl. HH.mm")}"
+                    };
+                    if (saveList.ShowDialog() == true)
+                    {
+                        StreamWriter writer = new StreamWriter(saveList.OpenFile());
+                        writer.WriteLine($"Plocklistan skapad den {DateTime.Now.ToString("yyyy.MM.dd kl. HH:mm")}", false);
+                        writer.WriteLine("\n\n");
+                        writer.WriteLine($"Varor att Plocka:");
+
+                        for (int i = 0; i < _orderList.Count; i++)
+                        {
+                            writer.WriteLine(_orderList[i].ToString());
+                        }
+
+                        writer.Dispose();
+                        writer.Close();
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("There was a problem saving your orderlist!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
 
-        private void RemoveFromCartButton_Click(object sender, RoutedEventArgs e) {
+        private void RemoveFromCartButton_Click(object sender, RoutedEventArgs e)
+        {
 
-            if (CartListBox.SelectedIndex >= 0) {
-                if (_orderList[CartListBox.SelectedIndex].Quantity > 1) {
+            if (CartListBox.SelectedIndex >= 0)
+            {
+                if (_orderList[CartListBox.SelectedIndex].Quantity > 1)
+                {
                     AdjustItemsQuantity(_orderList[CartListBox.SelectedIndex]);
                     _orderList[CartListBox.SelectedIndex].Quantity--;
                     CartListBox.Items.Clear();
 
-                    foreach (var item in _orderList) {
+                    foreach (var item in _orderList)
+                    {
                         CartListBox.Items.Add(item);
                     }
                 }
-                else {
+                else
+                {
                     AdjustItemsQuantity(_orderList[CartListBox.SelectedIndex]);
                     _orderList.RemoveAt(CartListBox.SelectedIndex);
                     CartListBox.Items.RemoveAt(CartListBox.SelectedIndex);
@@ -97,20 +124,25 @@ namespace GruppArbete_OOP
             }
         }
 
-        private void AdjustItemsQuantity(Item item) {
+        private void AdjustItemsQuantity(Item item)
+        {
             _mainWindow.warehouse.WarehouseStorage[item.Identifier].Quantity++;
         }
 
-        private void BackToWarehouseButton_Click(object sender, RoutedEventArgs e) {
+        private void BackToWarehouseButton_Click(object sender, RoutedEventArgs e)
+        {
             _mainWindow.Show();
             this.Hide();
         }
 
-        private void ClearCartButton_Click(object sender, RoutedEventArgs e) {
+        private void ClearCartButton_Click(object sender, RoutedEventArgs e)
+        {
             CartListBox.Items.Clear();
 
-            foreach (var item in _orderList) {
-                while (item.Quantity > 0) {
+            foreach (var item in _orderList)
+            {
+                while (item.Quantity > 0)
+                {
                     AdjustItemsQuantity(item);
                     item.Quantity--;
                 }
